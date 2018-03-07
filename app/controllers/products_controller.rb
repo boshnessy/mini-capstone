0d9c4969dcaeb0745166c1e59c1c7a47b5de33e9
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-06 19:52:34 -0600

    create route displaying all products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,18 @@
 class ProductsController < ApplicationController
+  def transportation_products
+    product1 = Product.first
+
+    render json: product1.as_json
+  end
+
+  def show_products
+    products = Product.all
+    product_hashes = []
+
+    products.each do |product|
+      product_hashes << {name: product.name, price: product.price, image: product.image_url, description: product.description}
+    end
+
+    render json: product_hashes
+  end
 end
