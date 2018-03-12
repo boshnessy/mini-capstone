553ffe96ba1a79c6b1e9a87ee96a804df74996db
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-12 18:54:39 -0500

    make model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,32 @@
 class Product < ApplicationRecord
+  def as_json
+    {
+      id: id,
+      name: name,
+      price: price,
+      tax: tax,
+      total: total,
+      image: image_url,
+      description: description,
+      discount: is_discounted
+    }
+  end 
+
+  def is_discounted
+
+    if price < 2
+      return true
+    else
+      return false
+    end
+  
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
