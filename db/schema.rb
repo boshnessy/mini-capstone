f1a1935f09f62d96e4298bc9ee1dee686c872972
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-15 19:30:26 -0500

    create supplier model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313231949) do
+ActiveRecord::Schema.define(version: 20180316002403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20180313231949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "in_stock"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
