f8c0673240750f3c79c9158203a80b78a3f5c072
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-18 12:07:30 -0500

    add gem 'knock', generate knock:token_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   namespace :v1 do
     get '/all-products' => 'products#show_products'
     get '/bike' => 'products#bike'
