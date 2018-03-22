15055ffaafaf0e6caeb35b92f5fdab4817a37c94
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-21 20:25:48 -0500

    make exceptions for admin authentication

--- app/controllers/v4/products_controller.rb
+++ app/controllers/v4/products_controller.rb
@@ -1,5 +1,5 @@
 class V4::ProductsController < ApplicationController
-  before_action :authenticate_admin
+  before_action :authenticate_admin, except: [:index, :show]
 
   def index
     search_term = params[:search]
