2c0ec5b55ebacb188cb0fb6583cc6abb5075caee
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-25 10:22:24 -0500

    add has_many_through associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   has_many :category_products
   has_many :categories, through: :category_products
   has_many :carted_products
+  has_many :orders, through: :carted_products
   belongs_to :supplier
   has_many :orders
   has_many :images
