195fb8ace39744ece585f0e0ed939498a675ed7b
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-25 10:05:33 -0500

    make associations between CartedProduct, User, Product, and Order

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
   belongs_to :supplier
   has_many :orders
   has_many :images
