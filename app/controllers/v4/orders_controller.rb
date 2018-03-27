547aed9082249c7522f0a95d874772b859e0202b
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-26 19:52:46 -0500

    update order create action

--- app/controllers/v4/orders_controller.rb
+++ app/controllers/v4/orders_controller.rb
@@ -11,27 +11,52 @@ class V4::OrdersController < ApplicationController
   end
 
   def create
-    carted_products = CartedProduct.where(status: "carted")
+    carted_products = current_user.carted_products.where(status: "carted")
+    # inputs: carted_products
+    # outputs: all the items and their prices and their quantities all added up together
+    calculated_subtotal = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+    end
 
-    product = Product.find_by(id: params[:product_id])
-    calculated_subtotal = params[:quantity].to_i * product.price
-    tax_rate = 0.09
-    calculated_tax = calculated_subtotal * tax_rate
+    calculated_tax = calculated_subtotal * 0.09
     calculated_total = calculated_subtotal + calculated_tax
 
     order = Order.new(
       user_id: current_user.id,
-      # product_id: params[:product_id],
-      # quantity: params[:quantity],
       subtotal: calculated_subtotal,
       tax: calculated_tax,
       total: calculated_total
     )
-
     if order.save
+      carted_products.update_all(status: "purchased", order_id: order_id)
       render json: order.as_json # {message: "Order created successfully"}, status: :created
     else
       render json: {errors: order.errors.full_messages}, status: :unprocessible_entity
     end
   end
+  # def create
+  #   carted_products = CartedProduct.where(status: "carted")
+
+  #   product = Product.find_by(id: params[:product_id])
+  #   calculated_subtotal = params[:quantity].to_i * product.price
+  #   tax_rate = 0.09
+  #   calculated_tax = calculated_subtotal * tax_rate
+  #   calculated_total = calculated_subtotal + calculated_tax
+
+  #   order = Order.new(
+  #     user_id: current_user.id,
+  #     # product_id: params[:product_id],
+  #     # quantity: params[:quantity],
+  #     subtotal: calculated_subtotal,
+  #     tax: calculated_tax,
+  #     total: calculated_total
+  #   )
+
+  #   if order.save
+  #     render json: order.as_json # {message: "Order created successfully"}, status: :created
+  #   else
+  #     render json: {errors: order.errors.full_messages}, status: :unprocessible_entity
+  #   end
+  # end
 end
